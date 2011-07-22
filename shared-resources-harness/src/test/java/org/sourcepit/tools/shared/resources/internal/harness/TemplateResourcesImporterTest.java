b641c530f7bea76a5ef934a797a4d44140a922ff
Bernd Vogt <bernd.vogt@sourcepit.org>
2011-07-22 09:45:00 +0000

    renamed resources-templating -> shared-resources

--- shared-resources-harness/src/test/java/org/sourcepit/tools/shared/resources/internal/harness/TemplateResourcesImporterTest.java
+++ shared-resources-harness/src/test/java/org/sourcepit/tools/shared/resources/internal/harness/TemplateResourcesImporterTest.java
@@ -2,7 +2,7 @@
  * Copyright (C) 2007 Innovations Softwaretechnologie GmbH, Immenstaad, Germany. All rights reserved.
  */
 
-package org.sourcepit.tools.resources.templating.internal.harness;
+package org.sourcepit.tools.shared.resources.internal.harness;
 
 import java.io.File;
 
